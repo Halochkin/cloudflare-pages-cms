e2107a50430762a6af88284e2ccf34768be71969
Halochkin <maksgalochkin2@gmail.com>
2022-04-26 19:08:38 +0300

    log

--- functions/set/[postId].js
+++ functions/set/[postId].js
@@ -12,6 +12,6 @@ export async function onRequestPost({request, params: {postId: slug}, env}) {
   const formData = await FormDataPolyfish(request);
   await saveFile(slug, formData, "image", env, "IMG");
   console.log(slug, "is ready to set")
-  await env.POSTS.put(slug, JSON.stringify(formData), {metadata: {title: formData.title, timestamp: Date.now()}});
+  await env.POSTS.put(slug, JSON.stringify(formData), {metadata: {...formData, timestamp: Date.now()}});
   return new Response(JSON.stringify(formData));
 }
