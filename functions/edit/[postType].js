74ffc0694246e2e92a1af0e7d7f69b9166ac90f0
Halochkin <maksgalochkin2@gmail.com>
2022-04-27 16:04:52 +0300

    log

--- functions/edit/[postType].js
+++ functions/edit/[postType].js
@@ -1,12 +1,27 @@
 
+function printPost({name: slug, metadata: {title, timestamp}}) {
+  return `
+      <li>
+        <a href="/post/${slug}.html">${title} (timestamp: ${timestamp})</a>
+        <a href="/post/${slug}.json">(as json)</a><br>
+        <button onclick="editPost('${slug}')">edit</button><br>
+        <a href="/delete/${slug}">delete this post</a>
+      </li>`;
+}
+
+
+
 export async function onRequestPost({request, params: {postId: slug}, env}) {
 
 
   debugger
   const json = await(await fetch("/list")).json();
+  const txt = json.keys.map(printPost).join('\n');
+  debugger;
 
-  debugger
+  const h1 = document.querySelector("h1");
 
 
-  return new Response("edit mtfckr");
+
+  return new Response(json, {headers: {"Content-Type": "text/html"}});
 }
