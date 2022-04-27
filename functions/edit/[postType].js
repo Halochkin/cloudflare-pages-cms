84a547b19d32b03e657065215616badd89f23470
Halochkin <maksgalochkin2@gmail.com>
2022-04-27 15:58:00 +0300

    log

--- functions/edit/[postType].js
+++ functions/edit/[postType].js
@@ -7,9 +7,6 @@ export async function onRequestPost({request, params: {postId: slug}, env}) {
 
   debugger
 
-  const formData = await FormDataPolyfish(request);
-  await saveFile(slug, formData, "image", env, "IMG");
-  console.log(slug, "is ready to set")
-  await env.POSTS.put(slug, JSON.stringify(formData), {metadata: {...formData, timestamp: Date.now()}});
-  return new Response(JSON.stringify(formData));
+
+  return new Response("edit mtfckr");
 }
