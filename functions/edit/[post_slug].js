476f587437e08b31a8c257264a3ef376a47cf082
Halochkin <maksgalochkin2@gmail.com>
2022-04-27 18:36:46 +0300

    log

---
+++ functions/edit/[post_slug].js
@@ -0,0 +1,30 @@
+function makeMetabox(title, element) {
+  let res = `
+    <div class="meta_frame">
+     <div class="meta_header"><h2 class="meta_title">${title}</h2></div>
+    <div class="meta_input">
+     ${element}
+    </div>
+   </div>
+    `;
+  return res;
+}
+
+
+
+
+
+export async function onRequest({params: {post_slug}, env}) {
+
+  const post = await env.POSTS.get(post_slug);
+
+
+
+  const title = makeMetabox("Title ",
+    `<input type="text" name="title" pattern="[a-zA-Z]{1}.*" value=${post.title}>(title must start with a  character)<br>`);
+
+
+  return new Response(title, {headers: {"Content-Type": "text/html"}});
+
+
+}
