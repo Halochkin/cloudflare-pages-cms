f9b78e04fa8cd1be7cf245c10094c7556af9b084
Halochkin <maksgalochkin2@gmail.com>
2022-04-27 16:28:50 +0300

    log

--- functions/edit/[post_type].js
+++ functions/edit/[post_type].js
@@ -1,22 +1,20 @@
-
-// function printPost({name: slug, metadata: {title, timestamp}}) {
-//   return `
-//       <li>
-//         <a href="/post/${slug}.html">${title} (timestamp: ${timestamp})</a>
-//         <a href="/post/${slug}.json">(as json)</a><br>
-//         <button onclick="editPost('${slug}')">edit</button><br>
-//         <a href="/delete/${slug}">delete this post</a>
-//       </li>`;
-// }
-
+function printPost({name: slug, metadata: {title, timestamp}}) {
+  return `
+      <li>
+        <a href="/post/${slug}.html">${title} (timestamp: ${timestamp})</a>
+        <a href="/post/${slug}.json">(as json)</a><br>
+        <button onclick="editPost('${slug}')">edit</button><br>
+        <a href="/delete/${slug}">delete this post</a>
+      </li>`;
+}
 
 
 export async function onRequest({params: {post_type}, env}) {
 
-  const json = await(await fetch("/list")).json();
-  // const txt = json.keys.map(printPost).join('\n');
- const text = `<h1>Suka, ${post_type}</h1>`
+  const json = await (await POSTS.list()).json();
+  const txt = json.keys.map(printPost).join('\n');
+  // const text = `<h1>Suka, ${post_type}</h1>`
 
 
-  return new Response(text, {headers: {"Content-Type": "text/html"}});
+  return new Response(txt, {headers: {"Content-Type": "text/html"}});
 }
