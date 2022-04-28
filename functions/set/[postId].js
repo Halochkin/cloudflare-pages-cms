5290afbfd7eb3c656dd2af28cfb139d1df7bbaae
Halochkin <maksgalochkin2@gmail.com>
2022-04-28 17:02:00 +0300

    editor

--- functions/set/[postId].js
+++ functions/set/[postId].js
@@ -8,10 +8,8 @@ async function saveFile(slug, formData, postId, env, KV_FILETABLE_NAME) {
 }
 
 export async function onRequestPost({request, params: {postId: slug}, env}) {
-  console.log("hello sunshine", env);
   const formData = await FormDataPolyfish(request);
   await saveFile(slug, formData, "image", env, "IMG");
-  console.log(slug, "is ready to set")
   await env.POSTS.put(slug, JSON.stringify(formData), {metadata: {...formData, timestamp: Date.now()}});
   return new Response(JSON.stringify(formData));
 }
