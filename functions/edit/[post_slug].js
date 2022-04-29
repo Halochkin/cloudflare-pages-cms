fe236046b9e535161dfcb2ee052d5302ba58c036
Halochkin <maksgalochkin2@gmail.com>
2022-04-29 15:04:15 +0300

    editor

--- functions/edit/[post_slug].js
+++ functions/edit/[post_slug].js
@@ -139,8 +139,8 @@ h1 a {
 }
 </style>`;
 
-function printPost({name: slug, metadata: {title, timestamp}}) {
-  return `<li><a href=${slug}>${title}</a></li>`;
+function printPost({name: slug, metadata: {title, type}}) {
+  return `<li><a class="post_item" type=${type} href=${slug}>${title}</a></li>`;
 }
 
 function makeTabMenu(posts) {
@@ -187,8 +187,35 @@ function makeMetabox(title, element) {
 
 const template = `
 <script>
+const form = document.querySelector("form");
 
-console.log("ass");
+  window.makeSlug = function (el) {
+    const slug = el.value.toLowerCase().replaceAll(/[^a-z_\\s-]/g, "").replaceAll(/\\s/g, "_");
+    //todo check this slug against existing elements.
+    el.form.setAttribute("action", \`/set/${slug}\`);
+  }
+
+  function makeMetabox(title, element) {
+    let res = \`
+    <div class="meta_frame">
+     <div class="meta_header"><h2 class="meta_title">${title}</h2></div>
+    <div class="meta_input">
+     ${element}
+    </div>
+   </div>
+    \`;
+    return res;
+  }
+
+ const title = makeMetabox("Title ",
+    \`<input type="text" name="title" pattern="[a-zA-Z]{1}.*" onChange="makeSlug(this)">(title must start with a  character)<br>\`);
+
+[...document.querySelectorAll(".post_item")].map(item => item.addEventListener("click", function () {
+  const postType = this.getAttribute("type");
+  if (postType === "video")
+    form.innerHTML =  title ;
+
+}))
 </script>`
 
 export async function onRequest({params: {post_slug}, env}) {
