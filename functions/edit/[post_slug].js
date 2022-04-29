4bc14b7f33d895681677c82f12de53e5abf9a9d9
Halochkin <maksgalochkin2@gmail.com>
2022-04-29 15:08:41 +0300

    editor

--- functions/edit/[post_slug].js
+++ functions/edit/[post_slug].js
@@ -187,7 +187,7 @@ function makeMetabox(title, element) {
 
 const template = `
 <script>
-const form = document.querySelector("form");
+  const form = document.querySelector("form");
 
   window.makeSlug = function (el) {
     const slug = el.value.toLowerCase().replaceAll(/[^a-z_\\s-]/g, "").replaceAll(/\\s/g, "_");
@@ -195,20 +195,8 @@ const form = document.querySelector("form");
     el.form.setAttribute("action", \`/set/${slug}\`);
   }
 
-  function makeMetabox(title, element) {
-    let res = \`
-    <div class="meta_frame">
-     <div class="meta_header"><h2 class="meta_title">${title}</h2></div>
-    <div class="meta_input">
-     ${element}
-    </div>
-   </div>
-    \`;
-    return res;
-  }
 
- const title = makeMetabox("Title ",
-    \`<input type="text" name="title" pattern="[a-zA-Z]{1}.*" onChange="makeSlug(this)">(title must start with a  character)<br>\`);
+ const title = makeMetabox("Title ",  <input type="text" name="title" pattern="[a-zA-Z]{1}.*" onChange="makeSlug(this)">(title must start with a  character)<br>);
 
 [...document.querySelectorAll(".post_item")].map(item => item.addEventListener("click", function () {
   const postType = this.getAttribute("type");
