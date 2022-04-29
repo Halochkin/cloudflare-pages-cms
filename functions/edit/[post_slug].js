b106060874c2bf3c14a0e7a614a055905f32af83
Halochkin <maksgalochkin2@gmail.com>
2022-04-29 16:02:10 +0300

    editor

--- functions/edit/[post_slug].js
+++ functions/edit/[post_slug].js
@@ -174,12 +174,45 @@ const form = `
 
 
 
+const template = `<script type="text/template"> 
+
+   const form = document.querySelector("form");
+
+  window.makeSlug = function (el) {
+    const slug = el.value.toLowerCase().replaceAll(/[^a-z_\\s-]/g, "").replaceAll(/\\s/g, "_");
+    //todo check this slug against existing elements.
+    el.form.setAttribute("action", \`/set/${slug}/\`);
+  }
+
+  function makeMetabox(title, element) {
+    let res = \`
+    <div class="meta_frame">
+     <div class="meta_header"><h2 class="meta_title">${title}</h2></div>
+    <div class="meta_input">
+     ${element}
+    </div>
+   </div>
+    \`;
+    return res;
+  }
+
+ const title = makeMetabox("Title ",
+    \`<input type="text" name="title" pattern="[a-zA-Z]{1}.*" onChange="makeSlug(this)">(title must start with a  character)<br>\`);
+
+[...document.querySelectorAll(".post_item")].map(item => item.addEventListener("click", function () {
+  const postType = this.getAttribute("type");
+  if (postType === "video")
+    form.innerHTML =  title ;
+
+}))
+
+</script>`
+
 export async function onRequest({params: {post_slug}, env}) {
 
   const json = await env.POSTS.list();
 
-
-  const html = style + container + makeTabMenu(json) + form + `</div>` ;
+  const html = style + container + makeTabMenu(json) + form + `</div>` + template ;
 
 
 
