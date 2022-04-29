880e26b9e4d73eeebb947497c55e3cbed95dd800
Halochkin <maksgalochkin2@gmail.com>
2022-04-29 15:26:44 +0300

    editor

--- functions/edit/[post_slug].js
+++ functions/edit/[post_slug].js
@@ -172,46 +172,54 @@ const form = `
  </div>`;
 
 
-function makeMetabox(title, element) {
-  let res = `
-    <div class="meta_frame">
-     <div class="meta_header"><h2 class="meta_title">${title}</h2></div>
-    <div class="meta_input">
-     ${element}
-    </div>
-   </div>
-    `;
-  return res;
-}
 
 
-const template = `
-<script>
-  const form = document.querySelector("form");
-
-  window.makeSlug = function (el) {
-    const slug = el.value.toLowerCase().replaceAll(/[^a-z_\\s-]/g, "").replaceAll(/\\s/g, "_");
-    //todo check this slug against existing elements.
-    el.form.setAttribute("action", "/set/${slug}");
-  }
-
-
- const title = makeMetabox("Title ",  "<input type="text" name="title" pattern="[a-zA-Z]{1}.*" onChange="makeSlug(this)">(title must start with a  character)<br>");
-
-[...document.querySelectorAll(".post_item")].map(item => item.addEventListener("click", function () {
-  const postType = this.getAttribute("type");
-  if (postType === "video")
-    form.innerHTML =  title ;
-
-}))
-</script>`
+// const template = `
+// <script>
+//   const form = document.querySelector("form");
+//
+//   window.makeSlug = function (el) {
+//     const slug = el.value.toLowerCase().replaceAll(/[^a-z_\\s-]/g, "").replaceAll(/\\s/g, "_");
+//     //todo check this slug against existing elements.
+//     el.form.setAttribute("action", \`/set/${slug}\`);
+//   }
+//
+//   function makeMetabox(title, element) {
+//     let res = \`
+//     <div class="meta_frame">
+//      <div class="meta_header"><h2 class="meta_title">${title}</h2></div>
+//     <div class="meta_input">
+//      ${element}
+//     </div>
+//    </div>
+//     \`;
+//     return res;
+//   }
+//
+//  const title = makeMetabox("Title ",
+//     \`<input type="text" name="title" pattern="[a-zA-Z]{1}.*" onChange="makeSlug(this)">(title must start with a  character)<br>\`);
+//
+// [...document.querySelectorAll(".post_item")].map(item => item.addEventListener("click", function () {
+//   const postType = this.getAttribute("type");
+//   if (postType === "video")
+//     form.innerHTML =  title ;
+//
+// }))
+// </script>`
 
 export async function onRequest({params: {post_slug}, env}) {
 
   const json = await env.POSTS.list();
 
-  const html = style + container + makeTabMenu(json) + form + `</div>` + template;
 
+  const script = await fetch("/script");
+
+  let scriptHtml = "<script>" + script + "</script>";
+
+  const html = style + container + makeTabMenu(json) + form + `</div>` + scriptHtml ;
+
+
+  console.log(script);
 
   // const title = makeMetabox("Title ",
   //   `<input type="text" name="title" pattern="[a-zA-Z]{1}.*" value=${metadata.title}>(title must start with a  character)<br>`);
